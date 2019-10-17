b7e47023f6180fb5d1c273bac32dd76ae602e883
1036225283 <id512@126.com>
2019-10-17 22:00:05 +0800

    softmax loss

--- src/main/java/xws/test/SoftmaxTest.java
+++ src/main/java/xws/test/SoftmaxTest.java
@@ -13,12 +13,17 @@ public class SoftmaxTest {
     public static void main(String[] args) {
 
         SoftmaxLayer softmaxLayer = new SoftmaxLayer();
-        Tensor tensorInput = TenserUtil.createTensor(4);
+        Tensor tensorInput = TenserUtil.createTensor(5);
         tensorInput.show("input ");
         Tensor tensorOut = softmaxLayer.forward(tensorInput);
         tensorOut.show("out ");
-        FullNetWorkTest.expectMNIST(3,4);
-        UtilNeuralNet.e
+        double total = 0;
+        for (int i = 0; i < tensorOut.getWidth(); i++) {
+            total = total + tensorOut.get(i);
+
+        }
+        System.out.println(total);
+//        UtilNeuralNet.e
 
     }
 }
